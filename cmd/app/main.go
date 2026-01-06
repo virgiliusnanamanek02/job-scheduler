111fd9e261b3aeebb0328df26fca884e46038014
Virgilius Nana Manek <virgilius.nana.manek0719@gmail.com>
2026-01-06 18:03:46 +0700

    feat: invoke config to main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/virgiliusnanamanek02/job-scheduler/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello")
+
+	appConfig := config.NewConfig()
+
+	log.Printf("Loading configuration from:")
+	log.Printf("  - Env File: %s", appConfig.EnvFile)
+	log.Printf("  - DB Config: %s", appConfig.DBConfigFile)
+	log.Printf("  - Migration Dir: %s", appConfig.MigrationDir)
+
+	db, err := config.ConnectDB(appConfig.DBConfigFile)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Printf("Error closing database connection: %v", err)
+		}
+	}()
+
+	err = config.ApplyMigrationWithConfig(db, appConfig)
+	if err != nil {
+		log.Fatalf("Failed to apply migrations: %v", err)
+	}
+
+	log.Println("Application started successfully")
 }
